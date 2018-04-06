426748177d496b25ae8ad804a2223d5b6517001d
Kevin Nguyen <25444103+kevin-compsci@users.noreply.github.com>
2018-04-06 00:43:35 -0500

    Update CDynArray.c

--- Dynamic_Array/C/CDynArray.c
+++ Dynamic_Array/C/CDynArray.c
@@ -1,5 +1,6 @@
 //Kevin Nguyen
-//Basic program for C (C files) that implements a dynammic array for use. This array should act similar to an arraylist in java. 
+//Basic program for C (C files) that implements a dynammic array for use. This array should act similar to an arraylist in java, but may look similar to LinkedLists.
+//NOTE: Look up information on pointer basics in C/C++
 
 /*
 How to run (linux):
